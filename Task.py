d95e127b0928787cf9815f0efb7812f1a101708c
GuisandeGolfer <DGuisande873@gmail.com>
2019-05-16 14:34:31 -0700

    all files in

---
+++ Task.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 14:05:31 2019
+
+@author: delar
+"""
+"""
+class Task :
+    
+    def __init__(self,name,start_time,end_time,color,date):
+        self.name = name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.color = color
+        self.date = date
+        
+    def get_time_of_task(self):
+"""
